a2b22a9f20d46eca3e2977128f4b7a7530ae14de
steven <steven.song@mail.utoronto.ca>
2015-07-21 12:05:04 -0400

    Added form validation for the addProperty page

---
+++ static/js/pValidate.js
@@ -0,0 +1,22 @@
+// Property validator for the "addProperty.html" page
+
+jQuery.validator.setDefaults({
+    highlight: function(element) {
+        jQuery(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        jQuery(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'label label-danger',
+    errorPlacement: function(error, element) {
+        if (element.parent('.input-group').length) {
+        	// If there is a input-group to be found, then insert a danger label after the input-group
+            error.insertAfter(element.parent());
+        } else if (!element.siblings().filter('.label').length){
+            error.insertAfter(element);
+            console.log(element.siblings().filter('.label').length);
+        }
+    }
+    
+});
