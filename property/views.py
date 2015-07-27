16f14150c1eefdddf6237c1efe0255018f96dd47
steven <steven.song@mail.utoronto.ca>
2015-07-26 20:03:05 -0400

    image upload function completed for the add property page

--- property/views.py
+++ property/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
 from django.db import connection
+import os.path
+import datetime
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Create your views here.
 def home(request):
@@ -31,13 +35,30 @@ def addProperty(request):
         size = request.POST.get("size", "")
         text = request.POST.get("text", "")
         user = request.POST.get("user", "")
+        print len(request.FILES)
+        print datetime.date.today()
+        if (len(request.FILES) > 0):
+            fType = request.FILES['my-file-selector'].name.split('.', 2)[1]
+            # Files will be named like usernamejan02.jpg
+            if (fType != ""):
+                fType = '.' + fType
+            handle_uploaded_file(request.FILES['my-file-selector'], 
+                                 user + 
+                                 str(datetime.datetime.now()).translate(None, " :") + 
+                                 fType)
         
         cursor = connection.cursor()
         cursor.execute("SELECT id FROM auth_user WHERE username = %s", [user])
-        user_id = cursor.fetchone()
+        user_id = cursor.fetchone()[0]
         cursor.execute("INSERT INTO property_property (title, address, city, province, size, text, user_id, date_added) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-                       [title, address, city, province, size, text, user, ''])
+                       [title, address, city, province, size, text, user_id, datetime.datetime.now()])
         return HttpResponseRedirect('/')
+    
+def handle_uploaded_file(f, name):
+    destination = open(os.path.join(os.path.dirname(os.path.dirname(__file__)),'pub\\img\\' + name), 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
 
 def register(request):
     return render(request, "register.html", {})
