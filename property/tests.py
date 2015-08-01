e3753d6eb6db413681dab7d18888381541a598aa
steven <steven.song@mail.utoronto.ca>
2015-08-01 16:41:10 -0400

    began writing tests for the property listing page

--- property/tests.py
+++ property/tests.py
@@ -38,8 +38,8 @@ class TestAddProperty(TestCase):
     def test_add_property_not_logged_in(self):
         # Try to access the add property page without logging in
         c = Client()
-        request = c.get('/addProperty/')
-        result = re.match(r'^.*\/accounts\/login\/\?next=\/addProperty.*$', request.url)
+        response = c.get('/addProperty/')
+        result = re.match(r'^.*\/accounts\/login\/\?next=\/addProperty.*$', response.url)
         # Check to see if the url that the client was redirected to was in fact
         # the url for the login page
         self.assertNotEqual(result, None)
@@ -48,7 +48,7 @@ class TestAddProperty(TestCase):
         # See if successfully adding a property will redirect the user
         # to the home page
         c = Client()
-        request = c.post('/addProperty/', 
+        response = c.post('/addProperty/', 
                             {'title' : 'Mansion', 
                             'address' : '1000 Military Drive', 
                             'city' : 'Toronto', 
@@ -56,8 +56,44 @@ class TestAddProperty(TestCase):
                             'size' : '10000.0',
                             'text' : 'Test',
                             'user' : 'admin'})
-        result = re.match(r'^http:\/\/[a-z]*\/$', request.url)
+        result = re.match(r'^http:\/\/[a-z]*\/$', response.url)
         # Check to see if the user was redirected to the main page after
         # submitting a new property
         self.assertEqual(result, None)
+        
+class TestHomePage(TestCase):
     
+    def setUp(self):
+        # Set up a test factory (so that request objects can be created later on)
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='admin', email='admin@asdfasdf.com', password='123456') 
+        
+    def test_home_page_with_multiple_properties(self):
+        # Add a single property to the database
+        Property.objects.create(title='Mansion',
+                                address = '1000',
+                                city = 'Toronto',
+                                province = 'Ontario',
+                                size = '10000',
+                                text = 'Test',
+                                user = self.user)
+        Property.objects.create(title='Mansion 2',
+                                address = '1100',
+                                city = 'Toronto',
+                                province = 'Ontario',
+                                size = '10000',
+                                text = 'Test',
+                                user = self.user)
+        c = Client()
+        # Check to see if those two properties were added to the property 
+        # listing page
+        response = c.get('/')
+        # The view should show two properties
+        self.assertEqual(len(response.context['properties']), 2)
+        # There should only be 1 page overall
+        self.assertEqual(response.context['current_page'], 1)
+        self.assertEqual(response.context['total_page_number'], 1)
+    
+    def tearDown(self):
+        Property.objects.all().delete()
