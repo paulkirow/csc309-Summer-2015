f2384adb6db6cb2be5812f2e1fc9e833867f9708
Xufeng Wang <xufeng.yo.wang@gmail.com>
2015-08-02 17:43:48 -0400

    finished profile page

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
-from property.models import Review
+from property.models import Property, Review
 
 
 def profile(request, userid):
 
     context = {}
 
+    properties = Property.objects.filter(user__id=userid)
+    context["properties"] = properties
+
+    reviews = Review.objects.filter(user__id=userid)
+    context["reviews"] = reviews
+
     reviews = Review.objects.filter(property__user__id=userid)
     valid_ratings = [review.rating for review in reviews if review.rating]
     avg_rating = float(sum(valid_ratings)) / len(valid_ratings)
     context["avg_rating"] = avg_rating
-    print(avg_rating)
 
     return render(request, "profile.html", context)
